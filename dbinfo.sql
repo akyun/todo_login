138651a14810705723d025c61eb4ce60605c3203
akyun <aoishi0313@gmail.com>
2017-03-06 17:52:10 +0900

    second commit

---
+++ dbinfo.sql
@@ -0,0 +1,17 @@
+mysql -u root
+
+create database todo_login;
+grant all on dotinstall_todo_app.* to dbuser@localhost identified by 'password';
+
+use dotinstall_todo_login;
+
+create table todos (
+  id int not null auto_increment primary key,
+  state tinyint(1) default 0, /* 0:not finished, 1:finished */
+  title text
+);
+
+insert into todos (state, title) values
+(0, 'todo 0'),
+(0, 'todo 1'),
+(1, 'todo 2');
